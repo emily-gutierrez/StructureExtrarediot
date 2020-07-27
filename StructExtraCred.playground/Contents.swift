eaa3fb89f898539cefd3928b2bdb0a927af65166
emily-gutierrez <67969028+emily-gutierrez@users.noreply.github.com>
2020-07-27 02:02:27 -0500

    Add files via upload

---
+++ StructExtraCred.playground/Contents.swift
@@ -0,0 +1,93 @@
+//contact information
+//full name
+
+struct Name{
+    var first = " Alex"
+    var last = " Rose"
+}
+let MyName = Name()
+print ("My first name is\(MyName.first)")
+print ("My last name is\(MyName.last)")
+
+//number
+struct Number{
+    var Number = " (123)-444-5959"
+}
+let MyNumber = Number()
+print ("My phone number is\(MyNumber.Number)")
+
+//email
+
+struct Email{
+    var email = " janedoe22@oops.org"
+}
+let MyEmail = Email()
+print ("My email is\(MyEmail.email)")
+
+//ICE 1 info
+//ICE 1 Name
+
+struct ICE1Name{
+    var ICE1NameFirst = " Elizabeth"
+    var ICE1NameLast = " Grace"
+}
+let MyICE1Name = ICE1Name()
+print ("My Emergency Contacts first name is\(MyICE1Name.ICE1NameFirst)")
+print ("My Emergency Contacts last name is\(MyICE1Name.ICE1NameLast)")
+
+//relation
+
+struct RelationToICE{
+    var Relation = " Mother"
+}
+let MyRelation = RelationToICE()
+print ("My relation is that she is my\(MyRelation.Relation)")
+
+//number
+
+struct Ice1Number{
+    var Number = " (123)-467-8989"
+}
+let MyICE1Number = Ice1Number()
+print ("My first Emergency Contacts number is\(MyICE1Number.Number)")
+
+//email
+struct Ice1Email{
+    var Email = " janedoe88@oops.org"
+}
+let MyICE1Email = Ice1Email()
+print("My first Emergency Contacts email is\(MyICE1Email.Email)")
+
+//ICE 2 info
+//ICE 2 Name
+
+struct ICE2Name{
+    var ICE2NameFirst = " Mason"
+    var ICE2NameLast = " Grace"
+}
+let MyICE2Name = ICE2Name()
+print ("My Emergency Contacts first name is\(MyICE2Name.ICE2NameFirst)")
+print ("My Emergency Contacts last name is\(MyICE2Name.ICE2NameLast)")
+
+//relation
+
+struct RelationToICE2{
+    var Relation = " Father"
+}
+let MyRelation2 = RelationToICE2()
+print ("My relation is that he is my\(MyRelation2.Relation)")
+
+//number
+
+struct Ice2Number{
+    var Number = " (123)-490-1010"
+}
+let MyICE2Number = Ice2Number()
+print ("My second Emergency Contacts number is\(MyICE2Number.Number)")
+
+//email
+struct Ice2Email{
+    var Email = " johndoe67@oops.org"
+}
+let MyICE2Email = Ice2Email()
+print("My second Emergency Contacts email is\(MyICE2Email.Email)")
